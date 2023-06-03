7b8180a23dff14d39e632b2e3d2efbd557ba7ff2
ClaudiaRojasSoto <claudiarojassoto@gmail.com>
2023-06-02 22:29:46 -0400

    Add the code to index.js file to start doing the modularitation of the javascript code

---
+++ index.js
@@ -0,0 +1,67 @@
+import { DateTime } from 'https://cdn.skypack.dev/luxon';
+import Book from './modules/book.js';
+import BookManager from './modules/bookManager.js';
+import { displayDate } from './modules/dateManager.js';
+import { displayBooks } from './modules/bookListView.js';
+
+class BookList {
+  constructor() {
+    this.bookList = document.getElementById('book-list');
+    this.inputTitle = document.getElementById('title');
+    this.inputAuthor = document.getElementById('author');
+    this.inputButton = document.getElementById('add-button');
+    this.listButton = document.getElementById('contact-section');
+    this.newButton = document.getElementById('form-section');
+    this.books = [];
+    this.bookManager = new BookManager();
+    this.inputButton.addEventListener('click', (event) => this.addBooks(event));
+    this.displayBooks();
+
+    this.listLink = document.getElementById('list');
+    this.addLink = document.getElementById('new');
+    this.contactLink = document.getElementById('contact');
+
+    this.listLink.addEventListener('click', () => this.showSection('div-list'));
+    this.addLink.addEventListener('click', () => this.showSection('form-section'));
+    this.contactLink.addEventListener('click', () => this.showSection('contact-section'));
+
+    this.displayDate();
+  }
+
+  addBooks(event) {
+    event.preventDefault();
+    const title = this.inputTitle.value;
+    const author = this.inputAuthor.value;
+    if (title !== '' && author !== '') {
+      const book = new Book(Date.now(), title, author);
+      this.bookManager.addBook(book);
+    }
+    this.displayBooks();
+    this.inputAuthor.value = '';
+    this.inputTitle.value = '';
+  }
+
+  showSection = (sectionId) => {
+    const sections = document.getElementsByClassName('list-section');
+
+    for (let i = 0; i < sections.length; i += 1) {
+      const content = sections[i];
+      if (content.id === sectionId) {
+        content.classList.remove('hidden');
+      } else {
+        content.classList.add('hidden');
+      }
+    }
+  }
+
+  displayDate() {
+    displayDate('date');
+  }
+
+  displayBooks() {
+    displayBooks(this.bookList, this.bookManager);
+  }
+}
+
+const bookList = new BookList();
+bookList.displayBooks();
